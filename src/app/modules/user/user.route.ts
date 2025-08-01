410e5360d65e73ad891b055e0286c2cda2b347ac
Jaber <jaberriyan357@gmail.com>
2025-08-01 19:47:53 +0600

    Verify Token and protect route using middleware

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,11 +1,43 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { UserControllers } from "./user.controller";
 import { createUserZodSchema } from "./user.validation";
 import { validateSchema } from "../../middlewares/validateRequest";
+import jwt, { JwtPayload } from "jsonwebtoken"
+import AppError from "../../errorHelpers/AppError";
+import httpStatus from "http-status-codes"
+import { Role } from "./user.interface";
 
 const router = Router()
 
 router.post("/register", validateSchema(createUserZodSchema), UserControllers.createUser)
-router.get("/", UserControllers.getAllUsers)
+router.get("/all-users", (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const accessToken = req.headers.authorization
+
+        if (!accessToken) {
+            throw new AppError(httpStatus.NOT_FOUND, "Access Token Not Received Yet!")
+        }
+
+        const verifiedToken = jwt.verify(accessToken, "secret")
+
+        console.log(verifiedToken);
+
+        if (!verifiedToken) {
+            throw new AppError(httpStatus.UNAUTHORIZED, `You are not authorized ${verifiedToken}`)
+        }
+
+        if ((verifiedToken as JwtPayload).role !== Role.ADMIN || Role.SUPER_ADMIN) {
+            throw new AppError(httpStatus.FORBIDDEN, "You are not permitted to view this route data!")
+        }
+
+        console.log(verifiedToken);
+
+        next()
+    }
+    catch (error: any) {
+        next(error)
+    }
+
+}, UserControllers.getAllUsers)
 
 export const UserRoutes = router
