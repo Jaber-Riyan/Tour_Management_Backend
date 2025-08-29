c8bd93335081c911f12a44f19e1f0d543da1e427
Jaber <jaberriyan357@gmail.com>
2025-08-29 22:55:47 +0600

    Setup QueryBuilder in Get All Divisions API

--- src/app/modules/division/division.service.ts
+++ src/app/modules/division/division.service.ts
@@ -1,3 +1,5 @@
+import { QueryBuilder } from "../../utils/QueryBuilder";
+import { divisionSearchableFields } from "./division.constant";
 import { IDivision } from "./division.interface";
 import { Division } from "./division.model";
 
@@ -24,24 +26,33 @@ const createDivision = async (payload: IDivision) => {
     return division
 };
 
-const getAllDivisions = async () => {
-    const divisions = await Division.find({});
-    const totalDivisions = await Division.countDocuments();
+const getAllDivisions = async (query: Record<string, string>) => {
+    const queryBuilder = new QueryBuilder(Division.find(), query);
+
+    const divisions = queryBuilder
+        .search(divisionSearchableFields)
+        .filter()
+        .sort()
+        .fields()
+        .paginate()
+
+    const [data, meta] = await Promise.all([
+        queryBuilder.build(),
+        queryBuilder.getMeta()
+    ]);
+
     return {
-        data: divisions,
-        meta: {
-            total: totalDivisions
-        }
+        data,
+        meta
     }
-};
+}
+
 const getSingleDivision = async (slug: string) => {
     const division = await Division.findOne({ slug });
     return {
         data: division,
     }
 };
-
-
 
 const updateDivision = async (id: string, payload: Partial<IDivision>) => {
 
