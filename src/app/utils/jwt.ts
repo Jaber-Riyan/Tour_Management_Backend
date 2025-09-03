4918acbaad50e27877812d74652eed35333e0e54
Jaber <jaberriyan357@gmail.com>
2025-09-04 00:25:53 +0600

    JWT Verify Token Function Updated

--- src/app/utils/jwt.ts
+++ src/app/utils/jwt.ts
@@ -1,5 +1,6 @@
 import jwt, { JwtPayload, SignOptions } from "jsonwebtoken"
 import { envVars } from "../config/env"
+import AppError from "../errorHelpers/AppError"
 
 export const generateJwtToken = (payload: JwtPayload, secret: jwt.Secret | jwt.PrivateKey, expiresIn: string = envVars.JWT_ACCESS_EXPIRES) => {
     const accessToken = jwt.sign(payload, secret, {
@@ -10,9 +11,18 @@ export const generateJwtToken = (payload: JwtPayload, secret: jwt.Secret | jwt.P
     return accessToken
 }
 
-export const verifyToken = (token: string, secret: string) => {
+export const verifyToken = (token: string, secret: string): JwtPayload => {
+    try {
+        return jwt.verify(token, secret) as JwtPayload;
+    } catch (error: any) {
+        console.log("JWT ERROR:", error.name, error.message);
 
-    const verifiedToken = jwt.verify(token, secret)
-
-    return verifiedToken
-}
+        if (error.name === "TokenExpiredError") {
+            throw new AppError(401, "Token expired. Please login again.");
+        }
+        if (error.name === "JsonWebTokenError") {
+            throw new AppError(401, "Invalid token. Please login again.");
+        }
+        throw new AppError(401, "Unauthorized access");
+    }
+};
